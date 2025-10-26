5d630381643598500c7d52f7802162a6d81f3760
Curtis Netterville <curtis@netterville.com>
2025-10-25 20:40:59 -0500

    Initial commit for checkpoints

---
+++ Weather Station App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather Station App
+//
+//  Created by Curtis Netterville on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather Station App/Weather_Station_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Station_AppApp.swift
+//  Weather Station App
+//
+//  Created by Curtis Netterville on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Station_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
